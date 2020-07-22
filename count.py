047a838110baa8d5c8c2db3d14522c00647c3b9f
syth0le <chdan565@gamil.com>
2020-07-22 20:57:01 +0300

    Full EDA & N-gramms

--- count.py
+++ count.py
@@ -9,5 +9,6 @@ words = (word for line in fileinput.input(count, 'windows-1251')
          for word in re.findall(r'\w+', line.casefold()))
 
 with open('count.txt', 'w') as w:
+    w.write('0,1\n')
     for word, count in Counter(words).most_common():
-        w.write("{} {},".format(word, count))
+        w.write("{}, {}\n".format(word, count))
